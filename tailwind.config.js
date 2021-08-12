fe6e94b49c53f940533e9063b1db05a345a8fde4
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-08-12 14:48:39 +0800

    feat: add default colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: "jit",
   purge: [
     './public/**/*.html',
     './src/**/*.{js,jsx,ts,tsx}'
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'slate': '#D8E3E7',
+        'uranus': '#51C4D3',
+        'uranus-dim': '#126E82',
+        'uranus-dark': '#132C33',
+      },
+      fontFamily: {
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+        ubuntu: ['Ubuntu', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   variants: {
-    extend: {},
+    opacity: ['disabled']
   },
   plugins: [],
 }
